bffc0723c46fc5cb1579ffd3ac31f40d3a394fa5
soyuz43 <kebekad673@proton.me>
2025-06-12 15:47:14 -0500

    Modified vite.config and launchSettings

--- DreamTracker-Client/vite.config.js
+++ DreamTracker-Client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "https://localhost:5001",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
-})
+}));
